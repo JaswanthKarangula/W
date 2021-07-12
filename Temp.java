6be25a150221fcd0475e99bedf7944e3d8bd23d5
Jaswanth Karangula <49749604+JaswanthKarangula@users.noreply.github.com>
2021-07-12 10:50:16 +0530

    Update Temp.java

--- Temp.java
+++ Temp.java
@@ -1,44 +1,70 @@
 package Servlets;
 
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.mysql.cj.Session;
+
+import Manager.Manager;
+import Model.*;
 
 
-public class LogoutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    
-    public LogoutServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+
+public class PostAddServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            response.setContentType("text/html");
-    	
-    	//invalidate the session if exists
-    	HttpSession session = request.getSession(false);
-    	
-    	if(session != null){
-    		session.invalidate();
-    	}
-    	response.sendRedirect("login.html");
-    }
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		processRequest(request, response);
-	}
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            String postName=request.getParameter("postTitle").trim();
+            String postContent = request.getParameter("postContent").trim();
+           
+            RequestDispatcher dis = request.getRequestDispatcher("addPost.html");
+            HttpSession session=request.getSession();
+            // Add to database
+            Manager manager=new Manager();
+            if (manager.addPost(postName,postContent,(User)session.getAttribute("user"))) {
+                response.sendRedirect("home.jsp");
+            } 
+            else {
+            	PrintWriter out= response.getWriter();
+    			out.println("<font color=red>something is wrong.</font>");
+                dis.forward(request, response);
+            }
+            
+        } 
+        catch (Exception e) {
+           e.printStackTrace();
+        }
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		processRequest(request, response);
-		
-	}
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 
 }
